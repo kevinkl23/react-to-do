7787317a9cf791505260dcf65208b76edeb1f1e4
kevinkl23 <lo.yohaneskevin@gmail.com>
2024-03-07 19:27:56 +0200

    Added functionality except delete task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addItem = () => {
+
+    if(!task) {
+      alert('Enter Task');
+      return;
+    }
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: task
+    };
+
+    setTasks((prev) => [...prev, item]);
+
+    setTask('');
+  }
+
+  const deleteTask = (id) => {
+    // const newArray = tasks.filter(task => task.id !== id);
+
+    // setTasks(newArray);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do List</h1>
+
+      <input type="text" placeholder='Add Task' value={task} onChange={e=>setTask(e.target.value)}>
+      </input>
+
+      <button onClick={()=>addItem()}>Add</button>
+
+      <ul>
+        {tasks.map(task => {
+          return (
+            <li key={task.id}>{task.value} <button onClick={deleteTask(task.id)}>X</button></li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
